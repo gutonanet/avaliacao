3049d541b847b39728343edb8843b6d670c0cc82
Augusto Lemes <gutonanet@hotmail.com>
2019-03-27 20:54:28 -0300

    correções

--- src/main/java/br/com/augustolemes/avaliacao/AvaliacaoApplication.java
+++ src/main/java/br/com/augustolemes/avaliacao/AvaliacaoApplication.java
@@ -2,9 +2,10 @@ package br.com.augustolemes.avaliacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AvaliacaoApplication {
+public class AvaliacaoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoApplication.class, args);
